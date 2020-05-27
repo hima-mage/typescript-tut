3656ab822afd5c5a2aa524909198640a12f19cc0
isheikh-dev <ishiekh.dev.v1@gmail.com>
2020-05-27 10:05:21 +0200

    sect9 les5 ended well . by using deecorator to bind this to method instead of regular way

--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,70 @@
-class ProjectInput { 
-    public templateElement : HTMLTemplateElement; // template element to get project input
-    public hostElement : HTMLDivElement; // div element to host the app 
-    public element: HTMLFormElement; // form element 
-    public titleInputElement : HTMLInputElement
-    public descriptionInputElement : HTMLInputElement
-    public peopleInputElement : HTMLInputElement
-
-    constructor() {
-        this.templateElement =  document.getElementById('project-input')! as HTMLTemplateElement;
-        this.hostElement = document.getElementById('app')! as HTMLDivElement; 
-
-        const importedNode = document.importNode(this.templateElement.content , true ); // get template content
-        this.element = importedNode.firstElementChild as HTMLFormElement // set form element to equal to first form in template
-        this.element.id = 'user-input'
-
-        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement
-        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement
-        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement
-
-
-        this.configure()
-        this.attach() // this will insert the form into the app-div element
-        
-
-    }
-    // there prob with this key word into submithander - it'sn refer to the element submited - so we have to bind it
-    private submitHandler(event : Event) { 
-        event.preventDefault();
-        console.log(this.titleInputElement.value)
+// autobind decorator
+function autobind(
+  _: any,
+  _2: string,
+  descriptor: PropertyDescriptor
+) {
+    const originalMethod = descriptor.value
+    const adjDescription : PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this)
+            return boundFn
+        }
     }
 
-
-    private configure() { 
-        this.element.addEventListener('submit' , this.submitHandler.bind(this))
-    }
-
-    private attach() { 
-        this.hostElement.insertAdjacentElement('afterbegin', this.element)
-    }
-    
+    return adjDescription
 }
 
-const prjinput = new ProjectInput()
+// ProjectInput Class
+class ProjectInput {
+  public templateElement: HTMLTemplateElement; // template element to get project input
+  public hostElement: HTMLDivElement; // div element to host the app
+  public element: HTMLFormElement; // form element
+  public titleInputElement: HTMLInputElement;
+  public descriptionInputElement: HTMLInputElement;
+  public peopleInputElement: HTMLInputElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    ); // get template content
+    this.element = importedNode.firstElementChild as HTMLFormElement; // set form element to equal to first form in template
+    this.element.id = "user-input";
+
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.configure();
+    this.attach(); // this will insert the form into the app-div element
+  }
+  // there prob with this key word into submithander - it'sn refer to the element submited - so we have to bind it
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler);
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const prjinput = new ProjectInput();
