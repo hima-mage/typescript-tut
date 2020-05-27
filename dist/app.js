31fb93e46557f042330892789725082d54112003
isheikh-dev <ishiekh.dev.v1@gmail.com>
2020-05-27 08:23:08 +0200

    setup first project , create project ,projectstatus , state

--- dist/app.js
+++ dist/app.js
@@ -1,93 +1,28 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
-};
-function Logger(logString) {
-    return function (constructor) {
-        console.log(logString);
-        console.log(constructor);
-    };
-}
-function WithTemplate(template, hookId) {
-    return function (constructor) {
-        const hookEl = document.getElementById(hookId);
-        const p = new constructor();
-        if (hookEl) {
-            hookEl.innerHTML = template;
-            hookEl.querySelector('h1').textContent = p.name;
-        }
-    };
-}
-let Person = (() => {
-    let Person = class Person {
-        constructor() {
-            this.name = "Max";
-            console.log("creating person object");
-        }
-    };
-    Person = __decorate([
-        Logger('loggin'),
-        WithTemplate('<h1>My Person Object</h1>', 'app')
-    ], Person);
-    return Person;
-})();
-const pers = new Person();
-console.log(pers);
-function Log(target, peopertyName) {
-    console.log('Property decorator');
-    console.log(target, peopertyName);
-}
-function Log2(target, name, descriptor) {
-    console.log('Accessor decorator');
-    console.log(target);
-    console.log(name);
-    console.log(descriptor);
-}
-function Log3(target, name, descriptor) {
-    console.log('Method decorator');
-    console.log(target);
-    console.log(name);
-    console.log(descriptor);
-}
-function Log4(target, name, position) {
-    console.log('Parameter decorator');
-    console.log(target);
-    console.log(name);
-    console.log(position);
-}
-let Product = (() => {
-    class Product {
-        constructor(t, p) {
-            this.title = t;
-            this._price = p;
-        }
-        set price(val) {
-            if (val > 0) {
-                this._price = val;
-            }
-            else {
-                throw new Error('Invalid Price - should be positice');
-            }
-        }
-        getPriceWithTax(tax) {
-            return this._price * (1 + tax);
-        }
+var ProjectStatus;
+(function (ProjectStatus) {
+    ProjectStatus[ProjectStatus["Active"] = 0] = "Active";
+    ProjectStatus[ProjectStatus["Finished"] = 1] = "Finished";
+})(ProjectStatus || (ProjectStatus = {}));
+class Project {
+    constructor(id, title, description, people, status) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.people = people;
+        this.status = status;
     }
-    __decorate([
-        Log
-    ], Product.prototype, "title", void 0);
-    __decorate([
-        Log2
-    ], Product.prototype, "price", null);
-    __decorate([
-        Log3,
-        __param(0, Log4)
-    ], Product.prototype, "getPriceWithTax", null);
-    return Product;
-})();
+}
+class ProjectState {
+    constructor() {
+        this.listeners = [];
+        this.projects = [];
+    }
+    static getInstance() {
+        if (this.instance) {
+            return this.instance;
+        }
+        this.instance = new ProjectState();
+        return this.instance;
+    }
+}
--- src/app.ts
+++ src/app.ts
@@ -1,82 +1,27 @@
-function Logger(logString : string) { 
-    return function(constructor : Function) { 
-        console.log(logString)
-        console.log(constructor)
-    } 
+// Project Class
+enum ProjectStatus { Active , Finished}
+class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public people: number, 
+    public status : ProjectStatus
+ ) {}
 }
 
+class ProjectState { 
+    private listeners : any[] = [];
+    private projects : Project[] = [];
+    private static instance : ProjectState
 
+    private constructor() {}
 
-function WithTemplate(template : string , hookId : string) { 
-    return function(constructor : any)  { 
-        const hookEl = document.getElementById(hookId)
-        const p = new constructor()
-        if (hookEl) { 
-            hookEl.innerHTML = template
-            hookEl.querySelector('h1')!.textContent = p.name
+    static getInstance() { 
+        if(this.instance) {
+            return this.instance
         }
-    }
-}
-@Logger('loggin')
-@WithTemplate('<h1>My Person Object</h1>' , 'app')
-
-class Person {
-    name = "Max" 
-    constructor() { 
-        console.log("creating person object")
-    }
-}
-
-const pers = new Person()
-
-console.log(pers)
-
-// ----------
-
-
-function Log(target: any , peopertyName : string){
-    console.log('Property decorator')
-    console.log(target, peopertyName)
-}
-
-function Log2(target: any, name:string , descriptor : PropertyDescriptor) { 
-    console.log('Accessor decorator')
-    console.log(target)
-    console.log(name)
-    console.log(descriptor)
-}
-
-function Log3(target: any , name:string | Symbol , descriptor: PropertyDescriptor) { 
-    console.log('Method decorator')
-    console.log(target)
-    console.log(name)
-    console.log(descriptor)
-}
-
-function Log4(target: any , name:string | Symbol , position: number) { 
-    console.log('Parameter decorator')
-    console.log(target)
-    console.log(name)
-    console.log(position)
-}
-class Product { 
-    @Log
-    title : string; 
-    private _price : number;
-    @Log2
-    set price(val : number){ 
-        if (val > 0){
-            this._price = val
-        } else { 
-            throw new Error('Invalid Price - should be positice')
-        }
-    }
-    constructor(t: string, p: number) {
-        this.title = t;
-        this._price = p;
-    }
-    @Log3
-    getPriceWithTax(@Log4 tax: number) {
-        return this._price * (1 + tax);
+        this.instance = new ProjectState() ;
+        return this.instance
     }
 }
